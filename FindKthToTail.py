4fd759f1fc922da5fcc82b99ad829212d9b1ca77
sarah3598 <yuanshuo98@sina.com>
2019-04-20 13:36:12 +0800

    链表中倒数第k个结点

---
+++ FindKthToTail.py
@@ -0,0 +1,17 @@
+# -*- coding:utf-8 -*-
+# class ListNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.next = None
+
+class Solution:
+    def FindKthToTail(self, head, k):
+        res=[]
+        while head:
+            res.append(head)
+            head=head.next
+        if k>len(res) or k<1:
+            return
+        else:
+            return res[-k]
+        # write code here
